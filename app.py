cdb648f8d548639a57539dff5a0d37ca29dafd29
dgiridaran <58911376+dgiridaran@users.noreply.github.com>
2021-08-27 20:26:26 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,jsonify
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+@app.route('/all',methods = ["GET"])
+def india_indices_sensex():
+    df = pd.read_html('https://www.moneycontrol.com/markets/indian-indices')
+    scr = df[0]
+    to = len(scr)-1
+    all_json = []
+    for i in range(to):
+        all_json.append({"company":scr[0][i],
+                        "LTP":scr[1][i],
+                        "%Charge":scr[2][i],
+                        "Volume":scr[3][i]})
+    return jsonify(all_json), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
