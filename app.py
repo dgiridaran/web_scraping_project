0223406c96e26fd5bab691badf2c9714a76d1eab
dgiridaran <58911376+dgiridaran@users.noreply.github.com>
2021-08-28 10:27:58 +0530

    new change

--- app.py
+++ app.py
@@ -15,7 +15,11 @@ def india_indices_sensex():
         all_json.append({"company":scr[0][i],
                         "LTP":scr[1][i],
                         "%Charge":scr[2][i],
-                        "Volume":scr[3][i]})
+                        "Volume":scr[3][i],
+                        "Buy Price":scr[4][i],
+                        "Sell Price":scr[5][i],
+                        "Buy Qty":scr[6][i],
+                        "Sell Qty":scr[7][i]})
     return jsonify(all_json), 200
 
 if __name__ == "__main__":
